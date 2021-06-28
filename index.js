6e041407bc2ef46f7e41f862f207c4a5f546c4cd
steven chen <schen0096@gmail.com>
2021-06-28 13:26:55 -0400

    commit tree a4acb418f3ca942dd5cf60242ece2c0d25e398c4

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+
+  if (someNumber <= 400) {
+    return 'This one is on me!'
+  }
+
+  else if (someNumber > 2000 && someNumber < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (someNumber > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+scuberGreetingForFeet(199);
+
+scuberGreetingForFeet(2001);
+
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(someCity){
   // Write your code here!
+  const result = someCity === 'NYC' ? "Ok, sounds good." : "No go."
+  return result
 }
 
-function switchOnCharmFromTip(){
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+      break
+    case 'not as generous':
+      return "Thank you."
+      break
+    default:
+      return "Bye."
+  }
+
 }
+
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not as generous');
+switchOnCharmFromTip('thanks for everything');
+
